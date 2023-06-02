c71e11ff214d24e170e84ffa7a47432f22780041
Mansi Verma <98829045+Mansiverma88@users.noreply.github.com>
2023-06-02 14:16:29 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5dd9349881mshd5ca46fc09ccee9p16408djsnb1fd01a976d9",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "b250f1d48fb8ca03e5e31bbdfa8b2cad"; // enter your key from openweather API
